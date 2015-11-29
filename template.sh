24e6113658751fef0c67d81a1285913e9c540d49
Aleksandr Saraikin <alex@sourcestream.de>
2015-11-29 17:21:29 +0100

    Set template path to pwd by default

--- template.sh
+++ template.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-bundle install
+bundle install --quiet
 /usr/bin/env ruby - "$@" <<RUBY
 
 require 'highcore_sparkle'
@@ -9,7 +9,7 @@ require 'trollop'
 options = Trollop::options do
   opt :stack_definition, 'JSON-encoded stack definition', :type => :string, :required => true
   opt :template, 'Name of the stack template', :type => :string, :required => true
-  opt :template_path, 'Path to the template directory', :type => :string, :required => true
+  opt :template_path, 'Path to the template directory', :type => :string, :default => Dir.pwd
 end
 
 puts HighcoreSparkle::generate(options[:template_path], options[:template], options[:stack_definition])
